8df43854cd001dcfd0d1bd57c73accc41eb0c2cc
Maximilian Zauner <zaunermax@gmail.com>
2019-11-03 16:15:05 +0100

    feat(🎉): initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+## Personal Web server abstraction for Fyrtur blinds
+
+This is a small project which aims to make controlling my Fyrtur blinds as easy as calling a rest api.
+
+## Installation
+
+To install the necessary dependencies, go over to this project and follow the installation instructions there.
+
+Then you just need to install the lib via: `pip3 install pytradfri`.
+
+## Config
+
+You need a config file in the root of your project: `tradfri.conf`
+
+The config must be in this form:
+
+```
+{
+    "identity": "<identity>",
+    "key": "<pre-shared-key>",
+    "host": "<gateway-ip>"
+}
+```
+
+You also need to create an api user on your gateway. After you installed all dependencies from the `pytradfri` package, 
+you can simply use the now installed `coap-client` to create one.
+
+```
+coap-client -m post -u "Client_identity" -k "SECURITY_CODE" -e '{"9090":"IDENTITY"}' "coaps://IP_ADDRESS:5684/15011/9063"
+# SECURITY_CODE = the security code under the gateway
+# IDENTITY      = your api user
+```
---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, Response, json
+from pytradfri import Gateway
+from pytradfri.api.libcoap_api import APIFactory
+from pytradfri.util import load_json
+
+app = Flask(__name__)
+
+CONFIG_FILE = 'tradfri.conf'
+
+config = load_json(CONFIG_FILE)
+psk_id = config["identity"]
+psk = config["key"]
+host = config["host"]
+
+api_factory = APIFactory(host=host, psk_id=psk_id, psk=psk)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/blinds', methods=['PUT'])
+def handle_blinds():
+    action = request.args.get('action')
+
+    if action is None:
+        return Response(response='Error, no action defined.', status=400)
+
+    if action != 'open' and action != 'close':
+        return Response(response='Error, wrong action: [' + action + ']', status=400)
+
+    api = api_factory.request
+    gateway = Gateway()
+
+    devices_command = gateway.get_devices()
+    devices_commands = api(devices_command)
+    devices = api(devices_commands)
+
+    blinds = [dev for dev in devices if dev.has_blind_control]
+    state = 0
+
+    if action == 'close':
+        state = 100
+
+    for blind in blinds:
+        api(blind.blind_control.set_state(state))
+
+    return Response(status=204)
+
+
+@app.route('/blinds', methods=['GET'])
+def get_blind_status():
+    api = api_factory.request
+    gateway = Gateway()
+
+    devices_command = gateway.get_devices()
+    devices_commands = api(devices_command)
+    devices = api(devices_commands)
+
+    blinds = [dev for dev in devices if dev.has_blind_control]
+    status = {}
+
+    for blind in blinds:
+        status[blind.name] = {
+            'battery': blind.device_info.battery_level,
+            'position': blind.blind_control.blinds[0].current_cover_position
+        }
+
+    return Response(response=json.dumps(status), status=200, mimetype='application/json')
+
+
+if __name__ == '__main__':
+    app.run()
